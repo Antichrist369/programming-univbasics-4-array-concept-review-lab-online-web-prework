199921e3c4ca47b59154e061e21a21deeb0006cc
Anthony Molina <antonio-molina@att.net>
2019-10-24 03:49:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,19 +14,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  array = [1, 2, 3, 4, 5] 
   puts array.min 
   array.min
 end
 
 
 
-   # counter = 0
-    #array = [1,2,3,4,5]
      
-    #while array[counter] do
-     # puts array[counter]
-      #counter += 1
-    #end
+
 
 
